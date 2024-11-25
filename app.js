272e575d551d5327865f4ba3a2cc1fbbd4ea6e4f
aljojose2552 <aljojose2552@gmail.com>
2024-11-25 11:41:49 +0000

    .env and js added for  connection

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 
-const app = express();   
+const app = express();
+
 
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'Password@123',
-  database: 'shiftmangement'
+  database: 'shiftmanagement' 
 });
 
 
@@ -23,9 +24,13 @@ connection.connect((err) => {
 app.use(bodyParser.json());
 
 
+
+
 app.get('/', (req, res) => {
   res.json({ info: 'Shift Management API is running.' });
 });
+
+
 app.get('/shifts', (req, res) => {
   connection.query('SELECT * FROM Shifts', (err, results) => {
     if (err) {
@@ -33,19 +38,21 @@ app.get('/shifts', (req, res) => {
       return res.status(500).json({ error: 'Internal server error' });
     }
 
-    //query results as JSON
+   
     res.status(200).json(results);
   });
 });
 
 
 app.post('/shifts', (req, res) => {
-  
   const { shift_name, start_time, end_time } = req.body;
+
+  
   if (!shift_name || !start_time || !end_time) {
     return res.status(400).json({ error: 'Please provide shift_name, start_time, and end_time' });
   }
 
+ 
   const query = 'INSERT INTO Shifts (shift_name, start_time, end_time) VALUES (?, ?, ?)';
 
   connection.query(query, [shift_name, start_time, end_time], (err, results) => {
@@ -54,13 +61,14 @@ app.post('/shifts', (req, res) => {
       return res.status(500).json({ error: 'Internal server error' });
     }
 
- 
+   
     res.status(201).json({
       message: 'Shift created successfully',
       shift_id: results.insertId
     });
   });
 });
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
---
+++ db.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL!');
+});
